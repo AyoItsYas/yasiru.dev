2324ef00987d3b101766b7f291c40bf23b698e30
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2023-06-04 12:26:10 +0530

    Meta tags and minor changes to improve SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/_app.scss';
+import Head from 'next/head';
+
+import '@styles/_app.scss';
+
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
